2dec07da7a09c7e39b8b2e57c2750c2ba211af94
Martin Ueding <mu@martin-ueding.de>
2023-07-18 14:27:22 +0200

    Refactor tests

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -1,25 +1,31 @@
 from literature_file_renamer.names import clean_name
 
 
-def test_clean_name() -> None:
-    input = """
+examples = {
+    """
+
+  
+    1.Su, J. et al. RoFormer: Enhanced Transformer with Rotary Position Embedding. Preprint at https://doi.org/10.48550/arXiv.2104.09864 (2022).
+  
+
+""": "Su, J. et al. RoFormer. Enhanced Transformer with Rotary Position Embedding. (2022)",
+    """
 
   
     1.Feathers, M. Working Effectively with Legacy Code. (Pearson, 2004).
   
 
-"""
-    expected = "Feathers, M. Working Effectively with Legacy Code. (Pearson, 2004)"
-    assert clean_name(input) == expected
-
-
-def test_clean_name_preprint() -> None:
-    input = """
+""": "Feathers, M. Working Effectively with Legacy Code. (Pearson, 2004)",
+    """
 
   
     1.Dao, T., Fu, D. Y., Ermon, S., Rudra, A. & Ré, C. FlashAttention: Fast and Memory-Efficient Exact Attention with IO-Awareness. Preprint at https://doi.org/10.48550/arXiv.2205.14135 (2022).
   
 
-"""
-    expected = "Dao, T., Fu, D. Y., Ermon, S., Rudra, A. & Ré, C. FlashAttention. Fast and Memory-Efficient Exact Attention with IO-Awareness. (2022)"
-    assert clean_name(input) == expected
+""": "Dao, T., Fu, D. Y., Ermon, S., Rudra, A. & Ré, C. FlashAttention. Fast and Memory-Efficient Exact Attention with IO-Awareness. (2022)",
+}
+
+
+def test_clean_name() -> None:
+    for input, expected in examples.items():
+        assert clean_name(input) == expected
