c0a6de63c490564f67372d0126cd58e16f484959
Martin Ueding <mu@martin-ueding.de>
2023-07-18 13:24:18 +0200

    Get name replacement done

--- literature_file_renamer/names.py
+++ literature_file_renamer/names.py
@@ -1,2 +1,10 @@
+import re
+
+
 def clean_name(name: str) -> str:
-    
+    name = name.strip()
+    name = re.sub(r"^1\. ?", "", name)
+    name = re.sub(r":", ".", name)
+    name = re.sub(r"\.+$", "", name)
+    name = re.sub(r" Preprint at \S+", "", name)
+    return name
