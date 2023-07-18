294538bb42a17b7275545372c039b0c83eaa83d8
Martin Ueding <mu@martin-ueding.de>
2023-07-18 14:27:18 +0200

    Add --dry mode

--- literature_file_renamer/__main__.py
+++ literature_file_renamer/__main__.py
@@ -7,14 +7,16 @@ from .names import clean_name
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("paths", nargs="+", metavar="path", type=pathlib.Path)
+    parser.add_argument("--dry", action="store_true")
     options = parser.parse_args()
 
     for path in options.paths:
         new_stem = clean_name(path.stem)
         if new_stem != path.stem:
             new_path = path.with_stem(new_stem)
-            print(f"Renaming {new_stem}.")
-            path.rename(new_path)
+            print(f"Renaming `{path.stem.strip()}` to `{new_stem}`.")
+            if not options.dry:
+                path.rename(new_path)
 
 
 if __name__ == "__main__":
